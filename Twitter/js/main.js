ea80d217fbf170d010fe75af2d486d1d1ab29153
Alejandra Durán <alejandra.duran.marquez@gmail.com>
2016-10-21 16:40:48 -0500

    Twitter terminado y subida de carpeta de Trello

--- Twitter/js/main.js
+++ Twitter/js/main.js
@@ -1,7 +1,11 @@
 
 	function todoList(){
         var item = document.getElementById("todoInput").value;
-        var text = document.createTextNode(item);
+        if (item == ""|| item == null){
+        alert ("No has escrito ninguna tarea, ingresala por favor");
+       return false;
+   }
+        var text = document.createTextNode("  " + item + "  ");
         var newItem = document.createElement("li");
         newItem.setAttribute("class","cajita");
         var checkbox= document.createElement("input");
@@ -9,6 +13,21 @@
         checkbox.value=1;
         newItem.appendChild(checkbox);
         newItem.appendChild(text);
+         var trash = document.createElement("span");
+        trash.setAttribute("class","fa fa-trash fa-1x");
+        newItem.appendChild(trash);
         document.getElementById("todoList").appendChild(newItem);
+        checkbox.onclick = function (){
+            if(checkbox.checked == true){
+         text.parentElement.setAttribute("class","tachado"); 
+        }else {
+            text.parentElement.setAttribute("class","cajita");
+        }
     }
+        trash.onclick = function (){
+         newItem.parentElement.removeChild(newItem); 
+        }
+        
+}
+
 
